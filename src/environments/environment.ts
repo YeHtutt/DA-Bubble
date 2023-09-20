ebe3379ef8c38505428b079b0fe52685efe95646
StefanHerrmann1991 <sth1812@posteo.de>
2023-09-20 16:21:25 +0200

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'da-bubble-dffea',
+    appId: '1:780185020934:web:f7602f0a757e22aec93187',
+    storageBucket: 'da-bubble-dffea.appspot.com',
+    apiKey: 'AIzaSyAeb-mzkt7PIkcnhXr5KIMbH65pMS5BX7A',
+    authDomain: 'da-bubble-dffea.firebaseapp.com',
+    messagingSenderId: '780185020934',
+  },
+};
