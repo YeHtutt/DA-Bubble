7b86e20c98a1bd85a8546fd985edddf269df4613
Jörg Schmalgemeier <j.schmalgemeier@googlemail.com>
2023-09-21 19:51:38 +0200

    created project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,26 @@ import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './auth/login/login.component';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { ChannelsComponent } from './main/channels/channels.component';
+import { MainComponent } from './main/main.component';
+import { DirectMessagesComponent } from './main/direct-messages/direct-messages.component';
+import { DashboardComponent } from './main/dashboard/dashboard.component';
+import { HeaderComponent } from './main/header/header.component';
+import { SidenavComponent } from './main/sidenav/sidenav.component';
+import { ThreadsComponent } from './main/threads/threads.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignUpComponent
+    SignUpComponent,
+    ChannelsComponent,
+    MainComponent,
+    DirectMessagesComponent,
+    DashboardComponent,
+    HeaderComponent,
+    SidenavComponent,
+    ThreadsComponent
   ],
   imports: [
     BrowserModule,
