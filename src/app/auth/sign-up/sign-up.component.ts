cf3e073b4b8e7bd03d39d8927431bb8f57953ba5
YeHtutAung <yehaung.nov@gmail.com>
2023-09-24 15:42:29 +0200

    signup, login and guestlogin added

--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -9,15 +11,39 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class SignUpComponent {
   checked = false;
 
+  constructor(private authService: AuthenticationService, private router:Router) {
+
+  }
+
   signUpForm = new FormGroup({
     "name" : new FormControl('', [Validators.required]),
     "email": new FormControl('', [Validators.email, Validators.required]) ,
     "password" : new FormControl('', [Validators.required]), 
-    "confirmPassword" : new FormControl('', [Validators.required]),
+    "agreement": new FormControl(false, [Validators.requiredTrue])
   })
 
-  submit() {
+  get name() {
+    return this.signUpForm.get('name');
+  }
 
+  get email() {
+    return this.signUpForm.get('email');
+  }
+
+  get password() {
+    return this.signUpForm.get('password');
+  }
+
+
+  submit() {
+    if(!this.signUpForm.valid || !this.signUpForm.get('agreement').value) {
+      return;
+    }
+    const {name, email, password} = this.signUpForm.value;
+    this.authService.signUp(name, email, password)
+    .subscribe(() => {
+      this.router.navigate(['/main']);
+    })
   }
   
 
