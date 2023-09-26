823c1612b50b8188924bceed95e317dbec2c2d2c
Kevin Ammerman <127098309+KevinAmmerman@users.noreply.github.com>
2023-09-26 23:01:00 +0200

    working on routing for channels, messages, direct

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { DirectMessage } from './models/direct-message';
+import { ChannelMessagesComponent } from './main/channel-messages/channel-messages.component';
+import { DirectMessagesComponent } from './main/direct-messages/direct-messages.component';
 
 const routes: Routes = [
   { path: 'main', component: MainComponent },
+  {
+    path: 'main', component: MainComponent,
+    children: [
+      { path: 'channel', component: ChannelMessagesComponent },
+      { path: 'direct', component: DirectMessagesComponent }
+    ]
+  },
   { path: 'login', component: LoginComponent },
-  { path: 'sign-up', component: SignUpComponent },
+  { path: 'sign-up', component: SignUpComponent }
 
 ];
 
