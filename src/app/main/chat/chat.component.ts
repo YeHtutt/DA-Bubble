3a05cac70e062b6e6e2a71e78defa96e5fc2dd26
Jörg Schmalgemeier <j.schmalgemeier@googlemail.com>
2023-09-28 18:39:05 +0200

    show direct message html layout

--- src/app/main/chat/chat.component.ts
+++ src/app/main/chat/chat.component.ts
@@ -6,4 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent {
+  text: string = '';
+
+  sendMessage() {
+    // this.message.username = 'Kevin Ammerman'
+    // this.message.time = new Date();
+    // this.message.text = this.text;
+    // this.channelService.addMessageToChannel(this.message.toJSON());
+    // this.text = '';
+  }
 }
