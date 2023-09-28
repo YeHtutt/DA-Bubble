b4180283958024577be9a690d1011dc42fafcefd
Kevin Ammerman <127098309+KevinAmmerman@users.noreply.github.com>
2023-09-28 19:51:56 +0200

    #

--- src/app/main/chat/chat.component.ts
+++ src/app/main/chat/chat.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { Message } from 'src/app/models/channel';
+import { ChannelService } from 'src/app/services/channel.service';
 
 @Component({
   selector: 'app-chat',
@@ -6,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent {
+
+  id: string = '';
+  text: string = '';
+  message: Message = new Message()
+  messages$: Observable<any>;
+
+  constructor(private channelService: ChannelService) {
+    this.messages$ = this.channelService.getChannelMessages(this.id).pipe(
+      map((messages) => {
+        return this.sortByDate(messages);
+      }));
+  }
+
+  sortByDate(message: any) {
+    return message.sort((a: any, b: any) => {
+      const dateA = a.time.seconds * 1000;
+      const dateB = b.time.seconds * 1000;
+      return dateA - dateB;
+    });
+  }
+
+  sendMessage() {
+
+  }
 
 }
--- src/app/main/new-message/new-message.component.ts
+++ src/app/main/new-message/new-message.component.ts
@@ -12,35 +12,11 @@ export class NewMessageComponent {
 
   text: string = '';
   message: Message = new Message()
-  messages$: Observable<any>;
   id: string = '';
-  hasData: boolean = false;
 
-  // constructor(private channelService: ChannelService) {
-  //   this.messages$ = this.channelService.getChannelMessages(this.id).pipe(map((message) => {
-  //     return this.sortByDate(message);
-  //   }));
-  //   console.log(this.messages$)
-  // }
 
-  constructor(private channelService: ChannelService) {
-    this.messages$ = this.channelService.getChannelMessages(this.id).pipe(
-      tap((messages) => {
-        if (messages && messages.length > 0) {
-          this.hasData = true;
-        }
-      }), map((messages) => {
-        return this.sortByDate(messages);
-      }));
-  }
+  constructor(private channelService: ChannelService) {}
 
-  sortByDate(message: any) {
-    return message.sort((a: any, b: any) => {
-      const dateA = a.time.seconds * 1000;
-      const dateB = b.time.seconds * 1000;
-      return dateA - dateB;
-    });
-  }
 
   sendMessage() {
     this.message.username = 'Kevin Ammerman'
