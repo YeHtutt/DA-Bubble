0201423efe89f4c2ca60f2ffc28230d3608e13e0
Jörg Schmalgemeier <j.schmalgemeier@googlemail.com>
2023-09-29 19:23:44 +0200

    start create tree for direct messages in sidenav

--- src/app/main/sidenav/sidenav.component.ts
+++ src/app/main/sidenav/sidenav.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ChannelService } from 'src/app/services/channel.service';
+
 
 @Component({
   selector: 'app-sidenav',
@@ -10,7 +12,12 @@ export class SidenavComponent {
   showSidenav: boolean = true;
   @Input() channel_window: any;
 
-  constructor(private Route: ActivatedRoute, private router: Router) {}
+  constructor(private Route: ActivatedRoute, private router: Router, public channelService: ChannelService,) {}
+
+  toggleExpanded(node: any) {
+    // Code to toggle the expanded state of the node
+    this.channelService.treeControl.toggle(node);
+  }
 
 
   toggleSidenav() {
--- src/app/services/direct-message.service.ts
+++ src/app/services/direct-message.service.ts
@@ -1,9 +1,97 @@
 import { Injectable } from '@angular/core';
+import { Message } from '../models/channel';
+import { ChannelService } from 'src/app/services/channel.service';
+import { FlatTreeControl } from '@angular/cdk/tree';
+import { MatTreeFlatDataSource, MatTreeFlattener } from '@angular/material/tree';
+import {
+  Firestore, collection,
+  doc, onSnapshot,
+  addDoc, getDoc, updateDoc,
+  deleteDoc, orderBy,
+  where, query,
+  limit,
+  collectionData
+} from '@angular/fire/firestore';
+
+
+interface MessagesNode {
+  messageName: string;
+  messageId: string;
+  children?: MessagesNode[];
+}
+
+interface ExampleFlatNode {
+  expandable: boolean;
+  messageName: string;
+  level: number;
+}
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DirectMessageService {
 
-  constructor() { }
+  messageTree: MessagesNode[] = [];
+  themes: any;
+  unsubMessage: any;
+
+
+  constructor(public channelService: ChannelService,) {
+    this.unsubMessage = this.subMessageList();
+  }
+
+  private _transformer = (node: MessagesNode, level: number) => {
+    return {
+      expandable: !!node.children && node.children.length > 0,
+      messageName: node.messageName,
+      messageId: node.messageId,
+      level: level,
+    };
+  };
+
+
+  treeControl = new FlatTreeControl<ExampleFlatNode>(
+    node => node.level,
+    node => node.expandable,
+  );
+
+
+  treeFlattener = new MatTreeFlattener<MessagesNode, ExampleFlatNode>(
+    this._transformer,
+    node => node.level,
+    node => node.expandable,
+    node => node.children,
+  );
+
+
+  hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
+
+
+  public dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+
+
+  subMessageList() {
+    return this.unsubMessage = onSnapshot(this.channelService.getRef('users'), (list: any) => {
+      this.messageTree = [];
+      list.forEach((element: any) => {
+        const channelObj = this.setChannelObj(element.data(), element.id);
+        this.messageTree.push(channelObj);
+      });
+      this.themes = [{ channelName: 'Channels', children: this.messageTree }];
+      this.dataSource.data = this.themes;
+    });
+  }
+
+  setMessageObj(obj: any, docId: string): MessagesNode {
+    return new Message({
+      channelId: docId,
+      channelName: obj.channelName,
+      creatorId: obj.creatorId,
+      description: obj.description,
+      creationTime: obj.creationTime,
+      createdBy: obj.createdBy,
+      children: []
+    });
+  }
 }
