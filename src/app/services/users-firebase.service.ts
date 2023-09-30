379795226e50ce8e86a5fc259da3597bebd60f94
Kevin Ammerman <127098309+KevinAmmerman@users.noreply.github.com>
2023-09-30 11:05:28 +0200

    working on search func for new message

--- src/app/services/users-firebase.service.ts
+++ src/app/services/users-firebase.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@angular/core';
-import { Firestore, collection, addDoc } from '@angular/fire/firestore';
+import { Firestore, collection, addDoc, getDocs } from '@angular/fire/firestore';
+
+
+interface User {
+  name: string;
+  email: string;
+  docId?: string;
+  uid?: string;
+  photoURL?: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersFirebaseService {
+
 
   constructor(private firestore: Firestore) { }
 
@@ -13,7 +23,7 @@ export class UsersFirebaseService {
       const docRef = await addDoc(collection(this.firestore, 'users'), user);
       //console.log('user saved successfully - ID:', docRef.id);
       return docRef.id;
-    }catch (error) {
+    } catch (error) {
       console.error('Error of saving users in Firebase:', error);
       throw error;
     }
@@ -22,4 +32,26 @@ export class UsersFirebaseService {
   saveToLocalStorage(value: any) {
     localStorage.setItem('currentUser', value);
   }
+
+  async getUsers() {
+    const itemCollection = collection(this.firestore, 'users');
+    const usersArray: any[] = [];
+    const querySnapshot = await getDocs(itemCollection);
+    querySnapshot.forEach(doc => {
+      const users = this.setUserObject(doc.data(), doc.id);
+      usersArray.push(users);
+    });
+    return usersArray;
+  }
+
+  setUserObject(obj: any, id: string): User {
+    return {
+      name: obj.name || '',
+      email: obj.email || '',
+      docId: id || '',
+      uid: obj.id || '',
+      photoURL: obj.photoURL || ''
+    }
+  }
+
 }
