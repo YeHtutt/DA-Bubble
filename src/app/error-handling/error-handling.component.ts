8f146b3fc58bc864f8314ffc151655c9cb9d1e53
StefanHerrmann1991 <sth1812@posteo.de>
2023-10-02 13:06:05 +0200

    styling of create-channel-component ready

--- src/app/error-handling/error-handling.component.ts
+++ src/app/error-handling/error-handling.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-error-handling',
-  templateUrl: './error-handling.component.html',
-  styleUrls: ['./error-handling.component.scss']
+  template: `
+  <h2>{{title}}</h2>
+  <p>{{message}}</p>
+`
 })
-export class ErrorHandlingComponent {
 
+export class ErrorHandlingComponent {
+  title: string;
+  message: string;
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
+    this.title = data.title;
+    this.message = data.message;
+  }
 }
