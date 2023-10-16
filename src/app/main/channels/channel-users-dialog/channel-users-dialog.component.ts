d3ba1fdb085481df7559b967e77ff2596fd7931d
StefanHerrmann1991 <sth1812@posteo.de>
2023-10-16 13:27:07 +0200

    working chip dispaly

--- src/app/main/channels/channel-users-dialog/channel-users-dialog.component.ts
+++ src/app/main/channels/channel-users-dialog/channel-users-dialog.component.ts
@@ -1,39 +1,56 @@
-import { Component, Inject, HostListener, } from '@angular/core';
+import {Component, ElementRef, ViewChild, Inject} from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { UsersFirebaseService } from 'src/app/services/users-firebase.service';
 import { ChannelService } from 'src/app/services/channel.service';
 import { SearchService } from 'src/app/services/search.service';
 import { FirebaseUtilsService } from 'src/app/services/firebase-utils.service';
 import { UserProfile } from 'src/app/models/user-profile';
-
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteSelectedEvent, MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatChipInputEvent, MatChipsModule } from '@angular/material/chips';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import {LiveAnnouncer} from '@angular/cdk/a11y';
 
 @Component({
   selector: 'app-channel-users-dialog',
   templateUrl: './channel-users-dialog.component.html',
-  styleUrls: ['./channel-users-dialog.component.scss']
+  styleUrls: ['./channel-users-dialog.component.scss'],
 })
 export class ChannelUsersDialogComponent {
 
   channel = this.data.channel;
   selectedOption: string | undefined;
   inputOpened = false;
-  allUsers: any;
-  filteredUser: any = [];
   public search: string = '';
   searchOutput: boolean = false;
   text: string = '';
   showTagMenu: boolean = false;
   addedUsers: UserProfile[] = [];
-  
+  separatorKeysCodes: number[] = [ENTER, COMMA];
+  userCtrl = new FormControl('');
+  filteredUsers: Observable<any[]>;
+  allUsers: any = [];
+  users: any[] = [];
+  @ViewChild('userInput') userInput!: ElementRef<HTMLInputElement>;
+
+
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     private usersService: UsersFirebaseService,
-    private channelService: ChannelService,
-    private searchService: SearchService,
     private firebaseUtils: FirebaseUtilsService,
-    private dialogRef: MatDialogRef<ChannelUsersDialogComponent>
-  ) { }
+    private dialogRef: MatDialogRef<ChannelUsersDialogComponent>,
+    private announcer: LiveAnnouncer,
+  ) {
+
+    this.filteredUsers = this.userCtrl.valueChanges.pipe(
+      startWith(null),
+      map((user: string | null) => (user ? this._filter(user) : this.allUsers.slice())),
+    );
+
+  }
 
   ngOnInit() {
     this.getAllUsers();
@@ -51,8 +68,8 @@ export class ChannelUsersDialogComponent {
   }
 
   async getAllUsers() {
-    this.allUsers = await this.usersService.getUsers();
-    this.addUsers();
+    this.allUsers = await this.usersService.getUsers();  
+    console.log(this.allUsers) 
   }
 
 
@@ -63,17 +80,34 @@ export class ChannelUsersDialogComponent {
     });
   }
 
-  @HostListener('document:click', ['$event'])
-  onDocumentClick(event: MouseEvent) {
-    const target = event.target as HTMLElement;
-    if (!target.closest('input') && !target.closest('.searchOutputHeader')) {
-      this.searchOutput = false;
+  remove(name: string): void {
+    const index = this.users.findIndex((user: any) => user.name === name);
+  
+    if (index >= 0) {
+      this.users.splice(index, 1);
+      this.announcer.announce(`Removed ${name}`);
     }
   }
 
-  async searchData() {
-    const searchResult = await this.searchService.searchUsersAndChannels(this.search)
-    this.filteredUser = searchResult.filteredUser;
+  selected(event: MatAutocompleteSelectedEvent): void {
+    const selectedUser = event.option.value;
+    this.users.push(selectedUser);
+    this.userInput.nativeElement.value = '';
+    this.userCtrl.setValue(null);
+  
+    // Remove the selected user from the allUsers array
+    const index = this.allUsers.findIndex((user: any) => user.name === selectedUser.name);
+    if (index !== -1) {
+      this.allUsers.splice(index, 1);
+    }
+  }
+
+  private _filter(value: any): any[] {
+    if (typeof value !== 'string') {
+      return [];
+    }
+    const filterValue = value.toLowerCase();
+    return this.allUsers.filter((user: any) => user.name.toLowerCase().includes(filterValue));
   }
 
   toggleSearchOutput() {
@@ -82,12 +116,18 @@ export class ChannelUsersDialogComponent {
 
   openProfile(user: any) { console.log(user); }
 
-  addThisUser(index: number) {
-    this.addedUsers.push(this.filteredUser[index]);
-    console.log(this.addedUsers)
-  }
 
   closeDialog() {
     this.dialogRef.close();
+  }
+
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+    // Add our fruit
+    if (value) {
+      this.users.push(value);
+    }
+    // Clear the input value
+    event.chipInput!.clear();
   }
 }
