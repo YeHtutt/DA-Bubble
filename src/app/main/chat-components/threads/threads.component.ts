93573ddac686e799257821606b2970abc7ccc175
StefanHerrmann1991 <sth1812@posteo.de>
2023-10-21 13:07:46 +0200

    show user profile in threads and formatted first message

--- src/app/main/chat-components/threads/threads.component.ts
+++ src/app/main/chat-components/threads/threads.component.ts
@@ -40,8 +40,13 @@ export class ThreadsComponent {
 
 
 
-  getTimeOfDate(time: string) { 
-    
+  getTimeOfDate(timestamp: any) {
+    const date = new Date(timestamp.seconds * 1000);
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const formattedTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+    return formattedTime;
   }
+
 
 }
