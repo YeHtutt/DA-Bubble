9742ff25e7b95d7b501ab0c94a9dba828abe7e11
Kevin Ammerman <127098309+KevinAmmerman@users.noreply.github.com>
2023-10-29 15:36:16 +0100

    implementing upload of files in messages

--- src/app/services/file-storage.service.ts
+++ src/app/services/file-storage.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { FileUpload } from '../models/file-upload';
-import { finalize } from 'rxjs';
+import { Subscription, catchError, finalize, of, tap } from 'rxjs';
 import { MessageService } from './message.service';
 
 @Injectable({
@@ -9,30 +9,46 @@ import { MessageService } from './message.service';
 })
 export class FileStorageService {
 
+  uploadSubscription: Subscription = new Subscription();
+
   constructor(
     private storage: AngularFireStorage,
     private messageService: MessageService
     ) { }
+
+    ngOnDestroy() {
+      this.uploadSubscription?.unsubscribe();
+    }
 
   uploadFile(fileUpload: FileUpload) {
     const filePath = `uploads/${new Date().getTime()}_${fileUpload.file.name}`; 
     const fileRef = this.storage.ref(filePath);
     const uploadTask = this.storage.upload(filePath, fileUpload.file);
 
-    uploadTask.snapshotChanges().pipe(
-      finalize(() => {
-        fileRef.getDownloadURL().subscribe(downloadURL => {
-          fileUpload.url = downloadURL;
-          fileUpload.name = fileUpload.file.name;
-        });
-      })
+    this.uploadSubscription = uploadTask.snapshotChanges().pipe(
+      tap(snapshot => {
+        if(snapshot?.state === 'success') {
+          this.getDownloadURL(fileRef); 
+        }
+      }), catchError(error =>{
+        console.log(error);
+        return of(null)
+      })  
     ).subscribe();
   }
 
 
-  downloadURL(filePath: string) {
-    const ref = this.storage.ref(filePath);
-    return ref.getDownloadURL(); 
+  getDownloadURL(fileRef: any) {
+    fileRef.getDownloadURL().subscribe({next: (url: string) => {
+      console.log(url); 
+    },
+    error: (error: Error) => {
+      console.log(error)
+    },
+    complete: () => {
+      this.uploadSubscription.unsubscribe();
+    }
+  });
   }
 
 }
