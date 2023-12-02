f04d805297a6e24efb4778e0c47c4e5e9f54f8d6
StefanHerrmann1991 <sth1812@posteo.de>
2023-12-02 12:24:44 +0100

    implemented distributor comoponent 2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,16 @@
-// export const environment = {
-//   firebase: {
-//     projectId: 'da-bubble-dffea',
-//     appId: '1:780185020934:web:f7602f0a757e22aec93187',
-//     storageBucket: 'da-bubble-dffea.appspot.com',
-//     apiKey: 'AIzaSyAeb-mzkt7PIkcnhXr5KIMbH65pMS5BX7A',
-//     authDomain: 'da-bubble-dffea.firebaseapp.com',
-//     messagingSenderId: '780185020934',
-//     databaseURL: "https://da-bubble-dffea-default-rtdb.europe-west1.firebasedatabase.app"
-//   },
-// };
-
 export const environment = {
+  firebase: {
+    projectId: 'da-bubble-dffea',
+    appId: '1:780185020934:web:f7602f0a757e22aec93187',
+    storageBucket: 'da-bubble-dffea.appspot.com',
+    apiKey: 'AIzaSyAeb-mzkt7PIkcnhXr5KIMbH65pMS5BX7A',
+    authDomain: 'da-bubble-dffea.firebaseapp.com',
+    messagingSenderId: '780185020934',
+    databaseURL: "https://da-bubble-dffea-default-rtdb.europe-west1.firebasedatabase.app"
+  },
+};
+
+/* export const environment = {
   firebase: {
     apiKey: "AIzaSyBy5E5j_g2LkufuTQSHAKaDByWL-A3tNoU",
     authDomain: "da-bubble-a97af.firebaseapp.com",
@@ -20,4 +20,4 @@ export const environment = {
     appId: "1:658582190460:web:22b8dadc94101442c0ccf9",
     databaseURL: "https://da-bubble-a97af-default-rtdb.europe-west1.firebasedatabase.app"
   }
-};
+}; */
